a71f256eb73397b94e9e768114f32158da823a74
Robin <philoin88@gmail.com>
2025-03-22 23:42:55 +0900

    fix: 회원가입 Form 기본 작업

--- client/app/auth/signup.tsx
+++ client/app/auth/signup.tsx
@@ -1,20 +1,82 @@
 import { StyleSheet, View } from 'react-native';
 import InputField from '../../components/InputField';
 import FixedBottomCTA from '@/components/FixedBottomCTA';
+import { useState } from 'react';
 
 export default function SignupScreen() {
+  const [signupValues, setSignupValues] = useState({
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  });
+  const [error, setError] = useState({
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  });
+
+  const checkValidation = () => {
+    let isValid = true;
+
+    if (!signupValues.email) {
+      setError((prev) => ({ ...prev, email: '이메일을 입력해주세요.' }));
+      isValid = false;
+    }
+
+    if (!signupValues.password) {
+      setError((prev) => ({ ...prev, password: '비밀번호를 입력해주세요.' }));
+      isValid = false;
+    }
+
+    if (!signupValues.passwordConfirm) {
+      setError((prev) => ({
+        ...prev,
+        passwordConfirm: '비밀번호 확인을 입력해주세요.',
+      }));
+      isValid = false;
+    }
+
+    return isValid;
+  };
+
+  const handleChange = (key: string, value: string) => {
+    setSignupValues((prev) => ({ ...prev, [key]: value }));
+    setError((prev) => ({ ...prev, [key]: '' }));
+  };
+
+  const handleSubmit = () => {
+    console.log('signupValues', signupValues);
+
+    if (!checkValidation()) return;
+  };
+
   return (
     <>
       <View style={styles.container}>
-        <InputField label="이메일" placeholder="이메일을 입력해주세요." />
-        <InputField label="비밀번호" placeholder="비밀번호을 입력해주세요." />
+        <InputField
+          label="이메일"
+          placeholder="이메일을 입력해주세요."
+          value={signupValues.email}
+          error={error.email}
+          onChangeText={(text) => handleChange('email', text)}
+        />
+        <InputField
+          label="비밀번호"
+          placeholder="비밀번호을 입력해주세요."
+          value={signupValues.password}
+          error={error.password}
+          onChangeText={(text) => handleChange('password', text)}
+        />
         <InputField
           label="비밀번호 확인"
           placeholder="비밀번호 확인을 입력해주세요."
+          value={signupValues.passwordConfirm}
+          error={error.passwordConfirm}
+          onChangeText={(text) => handleChange('passwordConfirm', text)}
         />
       </View>
 
-      <FixedBottomCTA label="회원가입하기" onPress={() => {}} />
+      <FixedBottomCTA label="회원가입하기" onPress={handleSubmit} />
     </>
   );
 }
--- client/components/InputField.tsx
+++ client/components/InputField.tsx
@@ -11,19 +11,29 @@ import {
 interface InputFieldProps extends TextInputProps {
   label?: string;
   variant?: 'filled' | 'standard' | 'outlined';
+  error?: string;
 }
 
-function InputField({ label, variant = 'filled', ...props }: InputFieldProps) {
+function InputField({
+  label,
+  variant = 'filled',
+  error,
+  ...props
+}: InputFieldProps) {
   return (
     <View>
       {label && <Text style={styles.label}>{label}</Text>}
-      <View style={[styles.container, styles[variant]]}>
+      <View
+        style={[styles.container, styles[variant], error && styles.inputError]}
+      >
         <TextInput
           placeholderTextColor={colors.GRAY_500}
           style={styles.input}
           {...props}
         />
       </View>
+
+      {error && <Text style={styles.error}>{error}</Text>}
     </View>
   );
 }
@@ -51,6 +61,15 @@ const styles = StyleSheet.create({
     fontSize: 16,
     padding: 0,
     flex: 1,
+  },
+  inputError: {
+    borderColor: colors.RED_500,
+    borderWidth: 1,
+  },
+  error: {
+    fontSize: 12,
+    marginTop: 5,
+    color: colors.RED_500,
   },
 });
 
